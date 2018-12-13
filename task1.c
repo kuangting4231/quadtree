5eb091bee5fc48d259e034bf06565531df1d2878
kuangting4231 <45567559+kuangting4231@users.noreply.github.com>
2018-12-13 18:56:30 +0800

    task1  destroytree
    this is a try

---
+++ task1.c
@@ -0,0 +1,31 @@
+#include "stdio.h"
+#include "stdlib.h"
+#include "math.h"
+#include "writeTree.h"
+#include "treeStructure.h"
+#include "buildTree.h"
+
+void destroyTree(Node** quadtree)
+{
+    int i =0;
+    Node* node = *quadtree
+    if (NULL==node)
+    {
+        return;
+    }
+    else
+    {
+        for(i=0;i<4;i++)
+        {
+            destroyTree(&node->child[i])
+        }
+        free(node);
+            node = NULL;
+     }
+   node = NULL;
+
+}
+
+
+
+
